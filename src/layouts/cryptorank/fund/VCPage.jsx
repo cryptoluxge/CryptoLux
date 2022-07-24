04ea7ea64ab13d7ab8921c2d619be7e057d86251
Ghonghito <ecryptolux@gmail.com>
2022-07-24 12:45:38 +0400

    CryptoRank-ის განახლება

--- src/layouts/cryptorank/fund/VCPage.jsx
+++ src/layouts/cryptorank/fund/VCPage.jsx
@@ -8,86 +8,83 @@ import { IoIosArrowUp } from "react-icons/io"
 const VCPage = () => {
   const [vcData, setVcData] = useState([])
   const [vcInvestment, setVcInvestment] = useState([])
+  const [vcInvestmentCategories, setVcInvestmentCategories] = useState([])
   const [openInvestment, setOpenInvestment] = useState(false)
   const params = useParams()
 
-  async function getVcDataFunc() {
+  async function getInvestorData() {
     const getData = await getVCData(params.id)
     setVcData(getData)
     const getVcInvestment = await getVCInvestments(getData.id)
     setVcInvestment(getVcInvestment)
+    setVcInvestmentCategories(getData.categoriesDistribution.sort((a, b) => b.percentage - a.percentage))
   }
 
   useEffect(() => {
-    getVcDataFunc()
-
+    getInvestorData()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
-    <div>
-      {vcInvestment.length > 0 ? (
-        <div className='flex items-center bg-white dark:bg-darkCard rounded-lg p-3 mt-5'>
-          <div>
-            <div className='flex items-center gap-2'>
-              <img src={vcData.images["x150"]} alt={vcData.name} className="w-14" />
-              <p className='text-lightText dark:text-darkText text-2xl'>{vcData.name}</p>
-            </div>
-            <div className='flex mt-3 gap-2'>
-              <span className="inline-flex items-center justify-center px-2 py-0.5 text-xs font-medium text-white bg-gradient-to-br from-violet to-violetDark rounded">Tier {vcData.tier}</span>
-              <span className="inline-flex items-center justify-center px-2 py-0.5 text-xs font-medium text-white bg-gradient-to-br from-violet to-violetDark rounded">{vcData.category.name}</span>
-            </div>
-            <div className='mt-2 w-[300px]'>
-              <p className='text-lightText dark:text-darkText'>{vcData.shortDescription}</p>
-            </div>
-            <div className='flex items-center gap-2'>
-              <span className="inline-flex items-center justify-center px-2 py-0.5 text-md mt-2 font-semibold text-white bg-gradient-to-br from-violet to-violetDark rounded">გაკეთებული ინვესტიციები: {vcInvestment.length}</span>
-              <div onClick={() => setOpenInvestment(!openInvestment)}>
-                {openInvestment ? <IoIosArrowUp className='text-gray-900 dark:text-white mt-1 cursor-pointer' /> : <IoIosArrowDown className='text-grey-900 dark:text-white mt-1 cursor-pointer' />}
+    <div className='flex flex-col md:flex-row gap-2 justify-center mt-5'>
+      <div>
+        {vcInvestment.length > 0 ? (
+          <div className='flex justify-center p-3 bg-white dark:bg-darkCard rounded-lg'>
+            <div>
+              <div className='flex items-center gap-2'>
+                <img src={vcData.images ? vcData.images["x150"] : 'https://pbs.twimg.com/media/B5ZmXEZIUAAjj10.jpg'} alt={vcData.name} className="w-14" />
+                <p className='text-lightText dark:text-darkText text-2xl'>{vcData.name}</p>
               </div>
-            </div>
-            {openInvestment ? (
-              <div className='mt-2 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 w-full'>
-                {vcData.categoriesDistribution.map((x) => (
-                  <span key={x.name} className="inline-flex items-center justify-center px-2 py-0.5 text-sm font-medium text-white bg-gradient-to-br from-violet to-violetDark rounded">{x.name} ({x.count}) - {(x.percentage).toLocaleString("en-US")}%</span>
-                ))}
+              <div className='flex mt-3 gap-2'>
+                <span className="inline-flex items-center justify-center px-2 py-0.5 text-xs font-medium text-white bg-gradient-to-br from-violet to-violetDark rounded">Tier {vcData.tier}</span>
+                <span className="inline-flex items-center justify-center px-2 py-0.5 text-xs font-medium text-white bg-gradient-to-br from-violet to-violetDark rounded">{vcData.category.name}</span>
               </div>
-            ) : null}
+              <div className='mt-2 w-[300px]'>
+                <p className='text-lightText dark:text-darkText'>{vcData.shortDescription}</p>
+              </div>
+              <div onClick={() => setOpenInvestment(!openInvestment)} className='cursor-pointer bg-gradient-to-br from-violet to-violetDark rounded px-2 py-1 flex justify-between items-center gap-2 mt-2'>
+                <p className='text-white text-sm font-semibold'>გაკეთებული ინვესტიციები - {vcInvestment.length}</p>
+                <div>
+                  {openInvestment ? <IoIosArrowUp className='text-gray-900 dark:text-white cursor-pointer' /> : <IoIosArrowDown className='text-grey-900 dark:text-white mt-1 cursor-pointer' />}
+                </div>
+              </div>
+              {openInvestment ? (
+                <div className='mt-2 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-1 gap-2 w-full'>
+                  {vcInvestmentCategories.map((x) => (
+                    <span key={x.name} className="inline-flex items-center justify-center px-2 py-0.5 text-sm font-medium text-white bg-gradient-to-br from-violet to-violetDark rounded">{x.name} ({x.count}) - {(x.percentage).toLocaleString("en-US")}%</span>
+                  ))}
+                </div>
+              ) : null}
+            </div>
           </div>
-        </div>
-      ) : null}
-      {vcInvestment.length > 0 ? (
-        <div className="overflow-x-auto shadow-md mt-3">
-          <table className="w-full text-sm mt-2 text-left text-gray-500 dark:text-gray-400">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-darkCard dark:text-gray-400">
-              <tr>
-                <th scope="col" className="px-6 py-3">
-                  სახელი
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  სიმბოლო
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  კატეგორია
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  კაპიტალიზაცია
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  ნავაჭრი (24სთ)
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                vcInvestment.map((x) => (
+        ) : null}
+      </div>
+      <div>
+        {vcInvestment.length > 0 ? (
+          <div className="overflow-x-auto rounded-lg">
+            <table className="text-sm text-left text-gray-500 dark:text-gray-400">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-darkCard dark:text-gray-400">
+                <tr>
+                  <th scope="col" className="px-6 py-3">
+                    სახელი
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    კატეგორია
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    კაპიტალიზაცია
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    ნავაჭრი (24სთ)
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {vcInvestment.map((x) => (
                   <tr key={x.key} className="bg-white dark:bg-darkCard hover:bg-indigo-100 dark:hover:bg-darkBackground w-full">
                     <td className="flex items-center gap-2 px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
                       <img src={x.image["x150"]} alt={x.name} className="w-8" />
-                      {x.name}
-                    </td>
-                    <td className="px-6 py-4">
-                      {x.symbol}
+                      <a href={`https://cryptorank.io/price/${x.key}`} target='_blank' rel='noreferrer'>{x.symbol}</a>
                     </td>
                     <td className="px-6 py-4">
                       {x.category}
@@ -100,11 +97,12 @@ const VCPage = () => {
                     </td>
                   </tr>
                 ))
-              }
-            </tbody>
-          </table>
-        </div>
-      ) : null}
+                }
+              </tbody>
+            </table>
+          </div>
+        ) : null}
+      </div>
     </div>
   )
 }
--- src/layouts/cryptorank/fund/index.jsx
+++ src/layouts/cryptorank/fund/index.jsx
@@ -16,61 +16,71 @@ const Index = () => {
   }, [])
 
   return (
-    <div className="overflow-x-auto shadow-md sm:rounded-lg mt-3">
-      <table className="w-full shadow-md text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs shadow-md text-gray-700 uppercase bg-white dark:bg-darkCard dark:text-gray-400">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-
-            </th>
-            <th scope="col" className="px-6 py-3">
-              ტიპი
-            </th>
-            <th scope="col" className="px-6 py-3">
-              ცვლილება (24სთ)
-            </th>
-            <th scope="col" className="px-6 py-3">
-              კაპიტალიზაცია
-            </th>
-            <th scope="col" className="px-6 py-3">
-              ნავაჭრი
-            </th>
-            <th scope="col" className="px-6 py-3">
-              დომინირება
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.length > 0 ? (
-            data.map((x) => (
-              <tr key={x.slug} className="bg-white dark:bg-darkCard hover:bg-indigo-100 dark:hover:bg-darkBackground w-full">
-                <td className="flex items-center gap-2 px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
-                  <img src={x.images.x150} alt={x.name} className="w-8" />
-                  <NavLink to={x.slug}>
-                    {x.name}
-                  </NavLink>
-                </td>
-                <td className="px-6 py-4">
-                  {x.tier}
-                </td>
-                <td className={`px-6 py-4 ${(Number(x.avgPriceChange["24H"]) > 0 ? "text-green-500" : "text-red-500")}`}>
-                  {x.avgPriceChange["24H"]}%
-                </td>
-                <td className="px-6 py-4">
-                  ${nFormatter(x.marketCap)}
-                </td>
-                <td className="px-6 py-4">
-                  ${nFormatter(x.volume24h)}
-                </td>
-                <td className="px-6 py-4">
-                  {(x.dominance).toLocaleString("en-US")}%
-                </td>
-              </tr>
-            ))
-          ) : null}
-        </tbody>
-      </table>
+    <div className='flex flex-col text-center'>
+      <div className='mt-5'>
+        <p className='text-lightText dark:text-darkText text-md font-bold'>ინვესტორები და მათი პორტფოლიო</p>
+      </div>
+      <div className='flex justify-center'>
+        {data.length > 0 ? (
+          <div className="overflow-x-auto mt-3 rounded-lg">
+            <table className="text-sm text-left text-gray-500 dark:text-gray-400">
+              <thead className="text-xs text-gray-700 uppercase bg-white dark:bg-darkCard dark:text-gray-400">
+                <tr>
+                  <th scope="col" className="px-6 py-3">
+                    ინვესტორი
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    ტიპი
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    ცვლილება (24სთ)
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    კაპიტალიზაცია
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    ნავაჭრი
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    დომინირება
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.length > 0 ? (
+                  data.map((x) => (
+                    <tr key={x.slug} className="bg-white dark:bg-darkCard hover:bg-indigo-100 dark:hover:bg-darkBackground w-full">
+                      <td className="flex items-center gap-2 px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                        <img src={x.images ? x.images.native : 'https://pbs.twimg.com/media/B5ZmXEZIUAAjj10.jpg'} alt={x.name} className="w-8 rounded-full" />
+                        <NavLink to={x.slug}>
+                          {x.name}
+                        </NavLink>
+                      </td>
+                      <td className="px-6 py-4">
+                        {x.tier}
+                      </td>
+                      <td className={`px-6 py-4 ${(Number(x.avgPriceChange["24H"]) > 0 ? "text-green-500" : "text-red-500")}`}>
+                        {x.avgPriceChange["24H"]}%
+                      </td>
+                      <td className="px-6 py-4">
+                        ${nFormatter(x.marketCap)}
+                      </td>
+                      <td className="px-6 py-4">
+                        ${nFormatter(x.volume24h)}
+                      </td>
+                      <td className="px-6 py-4">
+                        {(x.dominance).toLocaleString("en-US")}%
+                      </td>
+                    </tr>
+                  ))
+                ) : null}
+              </tbody>
+            </table>
+          </div>
+        ) : null}
+      </div>
     </div>
+
   )
 }
 
